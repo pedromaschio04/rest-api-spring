a5422b747a574d60678c700ae0b54c0f69f7e0db
Pedro Maschio <pedrohenriquemaschio@gmail.com>
2024-08-22 15:53:15 -0300

    first commit

---
+++ src/main/java/br/com/pedro/Greeting.java
@@ -0,0 +1,25 @@
+package br.com.pedro;
+
+public class Greeting {
+	
+	private final int id;
+	private final String content;
+	
+	public Greeting(int id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+	
+	
+	
+}
